c63ac7c0a3becb69270b8ca12cab1e2261be85d9
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-01 18:41:12 -0700

    Update displayResults.php

--- displayResults.php
+++ displayResults.php
@@ -1,74 +1,57 @@
+<!--
+Author: Kaiser
+Original Author: Chris
+Date last edited: 11/1/2021
+-->
+
 <?php
-
 include('connectionData.txt');
-
 $conn = mysqli_connect($server, $user, $pass, $dbname, $port)
 or die('Error connecting to MySQL server.');
-
 ?>
 
 <html>
 <head>
   <title>Another Simple PHP-MySQL Program</title>
   <link rel="stylesheet" href="displayResultsStyle.css" />  
-  </head>
-  
-  <body bgcolor="white">
-  
-  
-  <hr>
-  
-  
-<?php
-  
+  </head>  
+  <body>
+  <section>
+<?php  
 $state = $_POST['state'];
-
 $state = mysqli_real_escape_string($conn, $state);
 // this is a small attempt to avoid SQL injection
 // better to use prepared statements
-
 $query = "SELECT DISTINCT firstName, lastName, city FROM customer WHERE state = ";
 $query = $query."'".$state."' ORDER BY 2;";
-
 ?>
 
 <p>
 The query:
 <p>
+
 <?php
 print $query;
 ?>
 
-<hr>
 <p>
 Result of query:
 <p>
 
 <?php
-$result = mysqli_query($conn, $query)
-or die(mysqli_error($conn));
-
+$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 print "<pre>";
 while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-  {
-    print "\n";
-    print "$row[firstName]  $row[lastName] $row[city]";
-  }
+{
+  print "\n";
+  print "$row[firstName]  $row[lastName] $row[city]";
+}
 print "</pre>";
-
 mysqli_free_result($result);
-
 mysqli_close($conn);
-
 ?>
 
-<p>
-<hr>
-
-<p>
-<a href="displayResults.txt" >Contents</a>
-of the PHP program that created this page. 	 
- 
+<p> <a href="displayResults.txt" >Contents</a> of the PHP program that created this page. </p>
+</section>
 </body>
-</html>
-	  
+</html>	  
