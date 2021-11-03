47025d3e9a19d23dafa086a6b5654055cc7016a6
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-02 19:55:52 -0700

    fixed incorrect sql statement

--- displayResults.php
+++ displayResults.php
@@ -22,18 +22,18 @@ $state = $_POST['state'];
 $state = mysqli_real_escape_string($conn, $state);
 // this is a small attempt to avoid SQL injection
 // better to use prepared statements
-$query = "SELECT c.fname, c.lname, s.description
+$query = "SELECT distinct c.fname, c.lname, s.description
 from customer c
 inner join orders o on c.customer_num=o.customer_num
 inner join items i on o.order_num=i.order_num
 inner join stock s on i.manu_code=s.manu_code
 inner join manufact m on s.manu_code=m.manu_code
 where m.manu_name=";
-$query = $query."'".$state."';";
+$query = $query."'".$state."'order by c.fname, c.lname asc;";
 ?>
 
 <p> <h2>The query:</h2> <p>
-
+dis
 <?php
 print $query;
 ?>
