ad447e4e9f7ec9cdcce56758d04634416901d1fa
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-15 17:10:29 -0800

    Update wrap.php

--- tests/wrap.php
+++ tests/wrap.php
@@ -10,19 +10,14 @@ $state = $_POST['state'];
 $state = mysqli_real_escape_string($conn, $state);
 // this is a small attempt to avoid SQL injection
 // better to use prepared statements
-$query = "SELECT distinct c.fname, c.lname, s.description
+$query = "SELECT distinct c.fname, c.lname
 from customer c
-inner join orders o on c.customer_num=o.customer_num
-inner join items i on o.order_num=i.order_num
-inner join stock s on i.stock_num=s.stock_num and i.manu_code=s.manu_code
-inner join manufact m on s.manu_code=m.manu_code
-where m.manu_name=";
-$query = $query."'".$state."'order by c.fname, c.lname asc;";
+where c.fname="Frank";";
 
 
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-printf ("%s (%s) |%s|\n", $row[fname],  $row[lname], $row[description]);
+printf ("%s (%s) \n", $row[fname],  $row[lname]);
 echo "55";
 //echo $result;
 echo "1";
