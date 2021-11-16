4ff5a9ad6fb0458dcee85f72c7a177d2fc69bcf8
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-15 17:25:38 -0800

    Update wrap.php

--- tests/wrap.php
+++ tests/wrap.php
@@ -12,10 +12,7 @@ $output = wordwrap($text, 60, "<br>");
 // better to use prepared statements
 $query = "SELECT distinct c.fname, c.lname
 from customer c
-inner join orders o on c.customer_num=o.customer_num
-inner join items i on o.order_num=i.order_num
-inner join stock s on i.stock_num=s.stock_num and i.manu_code=s.manu_code
-inner join manufact m on s.manu_code=m.manu_code";
+where c.fname="Frank"";
 
 
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
