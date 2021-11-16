1391d43a74430b499e106d9e170f6140b920430a
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-15 17:15:18 -0800

    Update wrap.php

--- tests/wrap.php
+++ tests/wrap.php
@@ -6,25 +6,42 @@ or die('Error connecting to MySQL server.');
 
 
 
+$state = $_POST['state'];
+$state = mysqli_real_escape_string($conn, $state);
+// this is a small attempt to avoid SQL injection
+// better to use prepared statements
+$query = "SELECT distinct c.fname, c.lname, s.description
+from customer c
+inner join orders o on c.customer_num=o.customer_num
+inner join items i on o.order_num=i.order_num
+inner join stock s on i.stock_num=s.stock_num and i.manu_code=s.manu_code
+inner join manufact m on s.manu_code=m.manu_code
+where m.manu_name=";
+$query = $query."'".$state."'order by c.fname, c.lname asc;";
 
+
+$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+echo "55";
+//echo $result;
+echo "1";
+print "<pre>";
+echo "2";
+while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+{
+    echo "3";
+  //print "\n";
+  //print "$row[fname]  $row[lname] $row[description]";
+  echo "4";
+  echo "$row[fname]  $row[lname] $row[description]";
+  echo "5";
+}
+echo "6";
+print "</pre>";
+mysqli_free_result($result);
+mysqli_close($conn);
 
 $text = $_POST['text'];
 $output = wordwrap($text, 60, "<br>");
 echo $output;
-
-
-// this is a small attempt to avoid SQL injection
-// better to use prepared statements
-$query = "SELECT distinct c.fname, c.lname from customer c where c.fname="Frank"";
-
-
-$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-$row = mysqli_fetch_array($result, MYSQLI_BOTH);
-echo "55";
-printf ("%s (%s) \n", $row[fname],  $row[lname]);
-echo "55";
-mysqli_free_result($result);
-mysqli_close($conn);
-
 echo "kaisersucks";
 ?>
