fe4c40a264c4cbf33660b3630dcd1a0c1ec78daf
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-15 17:17:24 -0800

    Update wrap.php

--- tests/wrap.php
+++ tests/wrap.php
@@ -14,8 +14,22 @@ $query = "SELECT distinct c.fname, c.lname from customer c where c.fname="Frank"
 
 
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-$row = mysqli_fetch_array($result, MYSQLI_BOTH))
-
+echo "55";
+//echo $result;
+echo "1";
+print "<pre>";
+echo "2";
+while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+{
+    echo "3";
+  //print "\n";
+  //print "$row[fname]  $row[lname] $row[description]";
+  echo "4";
+  echo "$row[fname]  $row[lname] ";
+  echo "5";
+}
+echo "6";
+print "</pre>";
 mysqli_free_result($result);
 mysqli_close($conn);
 
