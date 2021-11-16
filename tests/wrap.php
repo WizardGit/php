db915a9bd9e02c3a89e511a0eca0f7158db10ede
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-15 17:12:14 -0800

    Update wrap.php

--- tests/wrap.php
+++ tests/wrap.php
@@ -6,34 +6,16 @@ or die('Error connecting to MySQL server.');
 
 
 
-$state = $_POST['state'];
-$state = mysqli_real_escape_string($conn, $state);
 // this is a small attempt to avoid SQL injection
 // better to use prepared statements
-$query = "SELECT distinct c.fname, c.lname
-from customer c
-where c.fname="Frank";";
+$query = "SELECT distinct c.fname, c.lname from customer c where c.fname="Frank";";
 
 
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+echo "55";
 printf ("%s (%s) \n", $row[fname],  $row[lname]);
 echo "55";
-//echo $result;
-echo "1";
-print "<pre>";
-echo "2";
-/*while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-{
-    echo "3";
-  //print "\n";
-  //print "$row[fname]  $row[lname] $row[description]";
-  echo "4";
-  echo "$row[fname]  $row[lname] $row[description]";
-  echo "5";
-}*/
-echo "6";
-print "</pre>";
 mysqli_free_result($result);
 mysqli_close($conn);
 
